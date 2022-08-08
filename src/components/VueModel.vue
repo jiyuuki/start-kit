a5217c27da1d33eaef52dafc7bee77041c107633
jiyuuki <salma.hajrawi@gmail.com>
2022-08-08 15:22:21 +0200

    VueModel

---
+++ src/components/VueModel.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- vue2 -->
+    <input
+      type="text"
+      :value="value"
+      @input="$emit(
+        'input',
+        $event.target.value
+      )"
+    >
+
+    <!-- vue3 -->
+    <input
+      type="text"
+      :value="modelValue"
+      @input="$emit(
+        'update:modelValue',
+        $event.target.value
+      )"
+    >
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueModel',
+
+  props: {
+    value: {
+      type: [String, Number],
+      default: null
+    },
+    modelValue: {
+      type: [String, Number],
+      default: null
+    }
+  }
+}
+</script>
