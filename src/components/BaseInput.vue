2899ec6fb4dec43262808bc9eaad58be8d0abf75
jiyuuki <salma.hajrawi@gmail.com>
2022-08-08 15:38:53 +0200

    clear

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -25,7 +25,7 @@
 
 <script>
 export default {
-  name: 'VueModel',
+  name: 'BaseInput',
 
   props: {
     value: {
---
+++ src/components/TwoBaseInput.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <!-- vue3 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TwoBaseInput',
+}
+</script>
